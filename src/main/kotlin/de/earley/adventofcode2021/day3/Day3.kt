a27e79ae961e917001fc16f1bad0fe48d09e31c0
Timothy Earley <timmy@earley.de>
2021-12-03 06:29:37 +0100

    Day 3

---
+++ src/main/kotlin/de/earley/adventofcode2021/day3/Day3.kt
@@ -0,0 +1,44 @@
+package de.earley.adventofcode2021.day3
+
+import de.earley.adventofcode2021.BaseSolution
+
+fun main() = Day3.start()
+
+object Day3 : BaseSolution<List<String>>() {
+
+	override fun parseInput(input: Sequence<String>): List<String> = input.toList()
+
+	override fun partOne(data: List<String>): Int {
+		val length = data.first().length
+
+		val positions = (0 until length).map { i ->
+			data.map { it[i] }
+		}
+
+		val gamma = positions.map { it.mostCommon() }.joinToString("").toInt(2)
+		val epsilon = positions.map { it.leastCommon() }.joinToString("").toInt(2)
+
+		return gamma * epsilon
+	}
+
+	override fun partTwo(data: List<String>): Int {
+		val oxygen = data.findValue { mostCommon() }
+		val co2 = data.findValue { leastCommon() }
+
+		return oxygen * co2
+	}
+
+	private fun List<String>.findValue(f: List<Char>.() -> Char): Int {
+		val candidates = this.toMutableList()
+		var bitPosition = 0
+		while (candidates.size > 1) {
+			val pred = candidates.map { it[bitPosition] }.f()
+			candidates.retainAll { it[bitPosition] == pred }
+			bitPosition++
+		}
+		return candidates.single().toInt(2)
+	}
+
+	private fun List<Char>.mostCommon(): Char = if (count { it == '0' } > count { it == '1' }) '0' else '1'
+	private fun List<Char>.leastCommon(): Char = if (count { it == '0' } > count { it == '1' }) '1' else '0'
+}
