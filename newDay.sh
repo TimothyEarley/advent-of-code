2b4ad2a47fc235eb6f6e0b600236a3d182eb9d7f
Timothy Earley <timmy@earley.de>
2021-12-05 21:50:59 +0100

    Expanded newDay script to download input data and task description

--- newDay.sh
+++ newDay.sh
@@ -1,28 +1,28 @@
-#! /bin/bash
+#! /bin/env bash
 
 set -euo pipefail
 
-# Create the files for a new day
-
-if [ -z "$1" ]; then
+function usage() {
   echo "Usage: $0 [day]"
   exit 1
-fi
+}
 
-day=$1
+function require() {
+  if ! command -v "$1" &>/dev/null; then
+    echo "$1 could not be found, please install"
+    exit 1
+  fi
+}
 
-if [ -f "src/main/kotlin/de/earley/adventofcode2021/day${day}/Day${day}.kt" ]; then
-  echo "Refusing to override files"
-  exit 1
-fi
-
-install -D /dev/null src/main/kotlin/de/earley/adventofcode2021/day"${day}"/Day"${day}".kt
-install -D /dev/null src/main/resources/de/earley/adventofcode2021/day"${day}"/input.txt
-install -D /dev/null src/test/kotlin/de/earley/adventofcode2021/day"${day}"/Day"${day}Test".kt
-install -D /dev/null src/test/resources/de/earley/adventofcode2021/day"${day}"/testInput.txt
-
-
-cat <<EOF > src/main/kotlin/de/earley/adventofcode2021/day"${day}"/Day"${day}".kt
+function createSolutionFile() {
+  f="src/main/kotlin/de/earley/adventofcode2021/day${day}/Day${day}.kt"
+  if [ -f "$f" ]; then
+    echo "Skipping solutions file (already exists)"
+    return
+  fi
+  echo "Creating solution file"
+  mkdir -p "$(dirname "$f")"
+  cat <<EOF >"$f"
 package de.earley.adventofcode2021.day${day}
 
 import de.earley.adventofcode2021.BaseSolution
@@ -31,16 +31,25 @@ fun main() = Day${day}.start()
 
 object Day${day} : BaseSolution<List<String>>() {
 
-	override fun parseInput(input: Sequence<String>): List<String> = input.toList()
+  override fun parseInput(input: Sequence<String>): List<String> = input.toList()
 
-	override fun partOne(data: List<String>): Int = 0
+  override fun partOne(data: List<String>): Int = 0
 
-	override fun partTwo(data: List<String>): Int = 0
+  override fun partTwo(data: List<String>): Int = 0
 
 }
 EOF
+}
 
-cat <<EOF > src/test/kotlin/de/earley/adventofcode2021/day"${day}"/Day"${day}Test".kt
+function createTestFile() {
+  f="src/test/kotlin/de/earley/adventofcode2021/day${day}/Day${day}Test.kt"
+  if [ -f "$f" ]; then
+    echo "Skipping test file (already exists)"
+    return
+  fi
+  echo "Creating test file"
+  mkdir -p "$(dirname "$f")"
+  cat <<EOF >"$f"
 package de.earley.adventofcode2021.day${day}
 
 import de.earley.adventofcode2021.testDay
@@ -49,5 +58,56 @@ import io.kotest.core.spec.style.WordSpec
 class Day${day}Test : WordSpec({
 	include(testDay(Day${day}, 0, 0))
 })
-
 EOF
+}
+
+function createInputFile() {
+  f="src/main/resources/de/earley/adventofcode2021/day${day}/input.txt"
+  if [ -f "$f" ]; then
+    echo "Skipping input file (already exists)"
+    return
+  fi
+  echo "Creating input file"
+  mkdir -p "$(dirname "$f")"
+  curl -sS --cookie "$cookieFile" "https://adventofcode.com/2021/day/{$day}/input" -o "$f"
+}
+
+function createTestInputFile() {
+  f="src/test/resources/de/earley/adventofcode2021/day${day}/testInput.txt"
+  mkdir -p "$(dirname "$f")"
+  touch "$f"
+}
+
+function createTaskFile() {
+  echo "Creating task file"
+
+  curl -sS --cookie "$cookieFile" "https://adventofcode.com/2021/day/{$day}" |
+    pup --pre '.day-desc' |
+    pandoc --from=html --to=gfm >src/main/kotlin/de/earley/adventofcode2021/day${day}/task.md
+
+}
+
+function main() {
+  require 'pup'
+  require 'pandoc'
+
+  createSolutionFile
+  createInputFile
+  createTestFile
+  createTestInputFile
+  createTaskFile
+}
+
+# get the input
+if [ $# -ne 1 ]; then
+  usage
+fi
+day=$1
+
+cookieFile=adventofcode.com_cookies.txt
+if [ ! -f "$cookieFile" ]; then
+  echo "The cookie file is missing! ($cookieFile)"
+  exit 1
+fi
+
+main
