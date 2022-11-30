d158e8daf7650ccce65070f7ed2eccc1b6850af2
Timothy Earley <timmy@earley.de>
2022-11-30 23:24:35 +0100

    RefreshVersions, no spotless

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,68 +1,56 @@
 plugins {
-    kotlin("jvm") version "1.6.0"
-    id("com.diffplug.spotless") version "6.0.0"
-    id("org.jetbrains.kotlinx.benchmark") version "0.3.1"
-    id("org.jetbrains.kotlin.plugin.allopen") version "1.6.0"
+	kotlin("jvm")
+	id("org.jetbrains.kotlinx.benchmark")
+	id("org.jetbrains.kotlin.plugin.allopen")
 }
 
 repositories {
-    mavenCentral()
+	mavenCentral()
 }
 
 dependencies {
-    testImplementation("io.kotest:kotest-runner-junit5:5.0.0")
+	testImplementation(Testing.kotest.runner.junit5)
 }
 
 tasks.test {
-    useJUnitPlatform()
-}
-
-spotless {
-    kotlin {
-        ktlint("0.43.0").userData(
-            mapOf(
-                "indent_style" to "tab",
-                "disabled_rules" to "parameter-list-wrapping"
-            )
-        )
-    }
+	useJUnitPlatform()
 }
 
 // Benchmark stuff
 
 allOpen {
-    annotation("org.openjdk.jmh.annotations.State")
+	annotation("org.openjdk.jmh.annotations.State")
 }
 
 sourceSets {
-    create("benchmark") {
-        compileClasspath += sourceSets.main.get().output
-        runtimeClasspath += sourceSets.main.get().output
-    }
+	create("benchmark") {
+		compileClasspath += sourceSets.main.get().output
+		runtimeClasspath += sourceSets.main.get().output
+	}
 }
 
 dependencies {
-    "benchmarkImplementation"("org.jetbrains.kotlinx:kotlinx-benchmark-runtime:0.3.1")
+	"benchmarkImplementation"("org.jetbrains.kotlinx:kotlinx-benchmark-runtime:_")
 }
 
 benchmark {
-    targets {
-        register("benchmark")
-    }
-    configurations {
-        getByName("main") {
-            warmups = 2
-            iterations = 5
-            iterationTime = 3
-            mode = "AverageTime"
-            outputTimeUnit = "MICROSECONDS"
-        }
-    }
+	targets {
+		register("benchmark")
+	}
+	configurations {
+		getByName("main") {
+			warmups = 2
+			iterations = 5
+			iterationTime = 3
+			mode = "AverageTime"
+			outputTimeUnit = "MICROSECONDS"
+		}
+	}
 }
 
 // Workaround for https://github.com/Kotlin/kotlinx-benchmark/issues/39
 afterEvaluate {
-    tasks.named<org.gradle.jvm.tasks.Jar>("benchmarkBenchmarkJar") {
-        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-    }
+	tasks.named<org.gradle.jvm.tasks.Jar>("benchmarkBenchmarkJar") {
+		duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+	}
 }
