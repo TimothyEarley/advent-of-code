03a2011b9898da3ddacde25b36b3aa7ef7934663
Timothy Earley <timmy@earley.de>
2022-12-01 06:11:07 +0100

    Day 1

---
+++ src/main/kotlin/de/earley/adventofcode2022/day1/Day1.kt
@@ -0,0 +1,20 @@
+package de.earley.adventofcode2022.day1
+
+import de.earley.adventofcode.BaseSolution
+import de.earley.adventofcode2021.split
+
+fun main() = Day1.start()
+
+object Day1 : BaseSolution<List<Int>, Int>() {
+
+	override fun parseInput(input: Sequence<String>): List<Int> =
+		input.toList()
+			.split { it.isBlank() }
+			.map { oneElf -> oneElf.sumOf { it.toInt() } }
+
+
+	override fun partOne(data: List<Int>): Int = data.max()
+
+	override fun partTwo(data: List<Int>): Int = data.sortedDescending().take(3).max()
+
+}
---
+++ src/test/kotlin/de/earley/adventofcode2022/day1/Day1Test.kt
@@ -0,0 +1,8 @@
+package de.earley.adventofcode2022.day1
+
+import de.earley.adventofcode.testDay
+import io.kotest.core.spec.style.WordSpec
+
+class Day1Test : WordSpec({
+	include(testDay(Day1, 24000, 45000))
+})
