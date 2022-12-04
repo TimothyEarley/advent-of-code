8336f055b7d59c43c8c1446611ee8a3ca8a055b8
Timothy Earley <timmy@earley.de>
2022-12-04 06:15:51 +0100

    Day 4

---
+++ src/main/kotlin/de/earley/adventofcode2022/day4/Day4.kt
@@ -0,0 +1,28 @@
+package de.earley.adventofcode2022.day4
+
+import de.earley.adventofcode.BaseSolution
+import de.earley.adventofcode2021.mapToList
+
+fun main() = Day4.start()
+
+object Day4 : BaseSolution<List<Pair<IntRange, IntRange>>, Int>() {
+
+	override fun parseInput(input: Sequence<String>): List<Pair<IntRange, IntRange>> = input.mapToList {
+		fun parseRange(s: String): IntRange = s
+			.split("-", limit = 2)
+			.let { (x, y) -> x.toInt()..y.toInt() }
+
+		it.split(",", limit = 2).map(::parseRange).let { (a, b) -> Pair(a, b) }
+	}
+
+	override fun partOne(data: List<Pair<IntRange, IntRange>>): Int = data.count { (a, b) ->
+		a.contains(b) || b.contains(a)
+	}
+
+	override fun partTwo(data: List<Pair<IntRange, IntRange>>): Int = data.count { (a, b) ->
+		(a intersect b).isNotEmpty()
+	}
+
+	private fun IntRange.contains(other: IntRange): Boolean = first <= other.first && other.last <= last
+
+}
---
+++ src/test/kotlin/de/earley/adventofcode2022/day4/Day4Test.kt
@@ -0,0 +1,8 @@
+package de.earley.adventofcode2022.day4
+
+import de.earley.adventofcode.testDay
+import io.kotest.core.spec.style.WordSpec
+
+class Day4Test : WordSpec({
+	include(testDay(Day4, 2, 4))
+})
