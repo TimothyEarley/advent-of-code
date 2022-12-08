06138d2cb145e2bd004d300e9de500f5b48fbbbf
Timothy Earley <timmy@earley.de>
2022-12-08 06:27:50 +0100

    Day 8

---
+++ src/main/kotlin/de/earley/adventofcode2022/day8/Day8.kt
@@ -0,0 +1,40 @@
+package de.earley.adventofcode2022.day8
+
+import de.earley.adventofcode.BaseSolution
+import de.earley.adventofcode2021.Grid
+import de.earley.adventofcode2021.Point
+import de.earley.adventofcode2021.manhattanDistanceTo
+
+fun main() = Day8.start()
+
+object Day8 : BaseSolution<Grid<Int>, Int>() {
+
+	override fun parseInput(input: Sequence<String>): Grid<Int> =
+		input.toList()
+			.let { list ->
+				Grid(list.first().length, list.size, list.flatMap { it.map(Char::digitToInt) })
+			}
+
+	override fun partOne(data: Grid<Int>): Int = data.indices.count { p ->
+		checkDir(p, data, Point(0, 1)).first ||
+				checkDir(p, data, Point(0, -1)).first ||
+				checkDir(p, data, Point(1, 0)).first ||
+				checkDir(p, data, Point(-1, 0)).first
+
+	}
+
+	override fun partTwo(data: Grid<Int>): Int = data.indices.maxOf { p ->
+		checkDir(p, data, Point(0, 1)).second * checkDir(p, data, Point(0, -1)).second *
+				checkDir(p, data, Point(1, 0)).second * checkDir(p, data, Point(-1, 0)).second
+	}
+
+	private fun checkDir(from: Point, data: Grid<Int>, dir: Point): Pair<Boolean, Int> {
+		val myHeight = data[from]!!
+		var n = from + dir
+		while (data.contains(n)) {
+			if (data[n]!! >= myHeight) return false to n.manhattanDistanceTo(from)
+			n += dir
+		}
+		return true to n.manhattanDistanceTo(from) - 1
+	}
+}
---
+++ src/test/kotlin/de/earley/adventofcode2022/day8/Day8Test.kt
@@ -0,0 +1,8 @@
+package de.earley.adventofcode2022.day8
+
+import de.earley.adventofcode.testDay
+import io.kotest.core.spec.style.WordSpec
+
+class Day8Test : WordSpec({
+	include(testDay(Day8, 21, 8))
+})
