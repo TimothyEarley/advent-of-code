45d843eabaee7b20b98d9a23db5b23542f95bafb
Timothy Earley <timothy.earley@deutschebahn.com>
2023-11-24 17:37:43 +0100

    updates

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 rootProject.name = "advent-of-code"
 
 plugins {
-	// See https://jmfayard.github.io/refreshVersions
-	id("de.fayard.refreshVersions") version "0.51.0"
+    // See https://jmfayard.github.io/refreshVersions
+    id("de.fayard.refreshVersions") version "0.60.3"
 }
