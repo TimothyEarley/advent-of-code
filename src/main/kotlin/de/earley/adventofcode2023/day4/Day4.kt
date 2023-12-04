4084ec788c219177641ff55ee83c469d8b225db7
Timothy Earley <timmy@earley.de>
2023-12-04 06:32:52 +0100

    Day 4

---
+++ src/main/kotlin/de/earley/adventofcode2023/day4/Day4.kt
@@ -0,0 +1,35 @@
+package de.earley.adventofcode2023.day4
+
+import de.earley.adventofcode.BaseSolution
+import kotlin.math.pow
+
+fun main() = Day4.start()
+
+object Day4 : BaseSolution<List<Day4.ScratchCard>, Int, Int>() {
+
+	override fun parseInput(input: Sequence<String>): List<ScratchCard> = input.mapIndexed { index, s ->
+		val s2 = s.split(":")[1]
+		val (winning, have) = s2.split("|", limit = 2)
+		ScratchCard(
+			winning.split(" ").map { it.trim() }.filter { it.isNotBlank() }.map { it.toInt() },
+			have.split(" ").map { it.trim() }.filter { it.isNotBlank() }.map { it.toInt() },
+		)
+	}.toList()
+
+	data class ScratchCard(val winning: List<Int>, val have: List<Int>) {
+		val correct : Int = have.count { it in winning }
+	}
+
+	override fun partOne(data: List<ScratchCard>): Int = data.sumOf { card ->
+		2.0.pow((card.correct - 1)).toInt()
+	}
+
+	override fun partTwo(data: List<ScratchCard>): Int = data.foldIndexed(data.map { 1 }) { index, countSoFar, scratchCard ->
+		val myCount = countSoFar[index]
+		countSoFar.mapIndexed { i, v ->
+			if (index + 1 <= i && i <= index + scratchCard.correct) v + myCount
+			else v
+		}
+	}.sum()
+
+}
