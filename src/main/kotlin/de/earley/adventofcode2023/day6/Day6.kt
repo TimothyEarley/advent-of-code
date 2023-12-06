aa6dc0bee2a50137406d6e5d5c912efb15c1343f
Timothy Earley <timothy.earley@deutschebahn.com>
2023-12-07 00:26:12 +0100

    Day 6: Look Ma, no loop!

--- src/main/kotlin/de/earley/adventofcode2023/day6/Day6.kt
+++ src/main/kotlin/de/earley/adventofcode2023/day6/Day6.kt
@@ -1,14 +1,18 @@
 package de.earley.adventofcode2023.day6
 
 import de.earley.adventofcode.BaseSolution
+import kotlin.math.ceil
+import kotlin.math.floor
+import kotlin.math.pow
+import kotlin.math.sqrt
 
 fun main() = Day6.start()
 
 object Day6 : BaseSolution<List<Day6.Race>, Long, Long>() {
 
 	override fun parseInput(input: Sequence<String>): List<Race> = input.toList().let { (time, dist) ->
-		val times = time.removePrefix("Time:").trim().split(" +".toRegex()).map { it.trim().toLong() }
-		val distances = dist.removePrefix("Distance:").trim().split(" +".toRegex()).map { it.trim().toLong() }
+		val times = time.removePrefix("Time:").trim().split(" +".toRegex()).map { it.toLong() }
+		val distances = dist.removePrefix("Distance:").trim().split(" +".toRegex()).map { it.toLong() }
 
 		times.zip(distances).map { (t, d) -> Race(t, d) }
 	}
@@ -17,12 +21,16 @@ object Day6 : BaseSolution<List<Day6.Race>, Long, Long>() {
 
 	override fun partOne(data: List<Race>): Long = data.map { it.waysToWin() }.reduce(Long::times)
 
-	private tailrec fun Race.waysToWin(speed: Int = 0, waysToWin: Long = 0): Long {
-		if (time <= 0) return waysToWin
-		// we still have time left, two options: go now or keep accelerating
-		val goNowDistance = time * speed
-		val goNowWin = if (goNowDistance > distance) 1 else 0
-		return this.copy(time = time - 1).waysToWin(speed + 1, waysToWin + goNowWin)
+	private fun Race.waysToWin(): Long {
+		// myDist = (time - x) * x = - x^2 + time * x
+		// myDist =? distance
+		// ==> x^2 - time * x + distance = 0
+		// x₁,₂ = time/2 ± sqrt( time^2 / 4 - distance )
+		// and then a bit of fiddling with rounding
+		val s = sqrt(time.toDouble().pow(2) / 4.0 - distance)
+		val x1 = floor(time / 2.0 - s)
+		val x2 = ceil(time / 2.0 + s - 1)
+		return (x2 - x1).toLong()
 	}
 
 	override fun partTwo(data: List<Race>): Long {
