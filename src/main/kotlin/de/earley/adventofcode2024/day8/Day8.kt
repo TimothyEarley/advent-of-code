038f9f7370b45761ac1f1df7868ffc19fa49c131
Timothy Earley <timmy@earley.de>
2024-12-08 06:13:16 +0100

    2024: Day 8 (inefficient)

---
+++ src/main/kotlin/de/earley/adventofcode2024/day8/Day8.kt
@@ -0,0 +1,69 @@
+package de.earley.adventofcode2024.day8
+
+import de.earley.adventofcode.BaseSolution
+import de.earley.adventofcode.Grid
+import de.earley.adventofcode.Point
+import de.earley.adventofcode.runUntil
+
+fun main() = Day8.start()
+
+object Day8 : BaseSolution<Grid<Char?>, Long, Long>() {
+
+	override fun parseInput(input: Sequence<String>): Grid<Char?> = input.toList().let { lines ->
+		Grid(lines.first().length, lines.size, lines.flatMap { line ->
+			line.map { char ->
+				when (char) {
+					'.' -> null
+					else -> char
+				}
+			}
+		})
+	}
+
+	override fun partOne(data: Grid<Char?>): Long {
+		var antiNodes = mutableSetOf<Point>()
+
+		data.pointValues()
+			.filter { it.second != null }
+			.forEach { (p1, char) ->
+			// find others with char
+			data.pointValues().filter { it.first != p1 && it.second == char}
+				.forEach { (p2, _) ->
+					val d = p2 - p1
+					val antiOne = p1 - d
+					val antiTwo = p2 + d
+					if (data.contains(antiOne)) {
+						antiNodes += antiOne
+					}
+					if (data.contains(antiTwo)) antiNodes += antiTwo
+				}
+		}
+
+		return antiNodes.size.toLong()
+	}
+
+	override fun partTwo(data: Grid<Char?>): Long {
+		var antiNodes = mutableSetOf<Point>()
+
+		data.pointValues()
+			.filter { it.second != null }
+			.forEach { (p1, char) ->
+				// find others with char
+				data.pointValues().filter { it.first != p1 && it.second == char}
+					.forEach { (p2, _) ->
+						val d = p2 - p1
+						runUntil(p1, { !data.contains(it) }) {
+							antiNodes.add(it)
+							it - d
+						}
+						runUntil(p2, { !data.contains(it) }) {
+							antiNodes.add(it)
+							it + d
+						}
+
+					}
+			}
+		return antiNodes.size.toLong()
+	}
+
+}
---
+++ src/test/kotlin/de/earley/adventofcode2024/day8/Day8Test.kt
@@ -0,0 +1,8 @@
+package de.earley.adventofcode2024.day8
+
+import de.earley.adventofcode.testDay
+import io.kotest.core.spec.style.WordSpec
+
+class Day8Test : WordSpec({
+	include(testDay(Day8, 14, 34))
+})
