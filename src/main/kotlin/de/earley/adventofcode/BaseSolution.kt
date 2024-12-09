c06fd9784639e5b3ebc7289609cde4456a7742db
Timothy Earley <timothy.earley@deutschebahn.com>
2024-12-09 07:13:24 +0100

    add duration to output

--- src/main/kotlin/de/earley/adventofcode/BaseSolution.kt
+++ src/main/kotlin/de/earley/adventofcode/BaseSolution.kt
@@ -1,20 +1,24 @@
 package de.earley.adventofcode
 
+import kotlin.time.TimedValue
+import kotlin.time.measureTimedValue
+
 abstract class BaseSolution<In, Out1, Out2> {
 
 	fun start() {
 		val day = this::class.java.simpleName.takeLastWhile(Char::isDigit).toInt()
 
 		val data = readResource("input.txt").useLines(this::parseInput)
-		val one = partOne(data)
-		val two = partTwo(data)
+		val one = measureTimedValue { partOne(data) }
+		val two = measureTimedValue { partTwo(data) }
 
 		printResult(one, two, day)
 	}
 
-	private fun printResult(one: Out1, two: Out2, day: Int) {
-		val width = maxOf(one.toString().length, two.toString().length)
-		val totalWidth = width + 11
+	private fun printResult(one: TimedValue<Out1>, two: TimedValue<Out2>, day: Int) {
+		val widthResult = maxOf(one.value.toString().length, two.value.toString().length)
+		val widthTime = maxOf(one.duration.toString().length, two.duration.toString().length)
+		val totalWidth = widthResult + widthTime + 14
 		val bar = "═".repeat(totalWidth)
 		val daySpacesLeft = (totalWidth - 6) / 2
 		val daySpaceLeft = " ".repeat(daySpacesLeft)
@@ -25,9 +29,9 @@ abstract class BaseSolution<In, Out1, Out2> {
 			╔$bar╗
 			║${daySpaceLeft}Day ${day.toString().padStart(2)}$daySpaceRight║
 			╠$bar╣
-			║ Part 1 ║ ${one.toString().padEnd(width)} ║
+			║ Part 1 ║ ${one.value.toString().padEnd(widthResult)} ║ ${one.duration.toString().padStart(widthTime)} ║
 			╠$bar╣
-			║ Part 2 ║ ${two.toString().padEnd(width)} ║
+			║ Part 2 ║ ${two.value.toString().padEnd(widthResult)} ║ ${two.duration.toString().padStart(widthTime)} ║
 			╚$bar╝
 			""".trimIndent()
 		)
