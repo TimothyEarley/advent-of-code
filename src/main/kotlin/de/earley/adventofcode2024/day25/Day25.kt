82743ad30e1f8f3bb67aaf30d109e412765b4c28
Timothy Earley <timothy.earley@deutschebahn.com>
2024-12-25 05:17:01 +0000

    2024: Day 25

---
+++ src/main/kotlin/de/earley/adventofcode2024/day25/Day25.kt
@@ -0,0 +1,59 @@
+package de.earley.adventofcode2024.day25
+
+import de.earley.adventofcode.BaseSolution
+import de.earley.adventofcode.split
+import de.earley.adventofcode.toGrid
+
+fun main() = Day25.start()
+
+object Day25 : BaseSolution<Day25.Input, Long, Long>() {
+
+	data class Input(
+		val locks: List<List<Int>>,
+		val keys: List<List<Int>>
+	)
+
+	override fun parseInput(input: Sequence<String>): Input = input
+		.toList()
+		.split { it.isBlank() }
+		.map { block ->
+			block.toGrid {
+				when (it) {
+					'#' -> true
+					'.' -> false
+					else -> error("")
+				}
+			}
+		}
+		.map { grid ->
+			if (grid.get(0, 0) == true) {
+				// lock
+				val heights = (0..<grid.width).map { x ->
+					(0..<grid.height).first { y -> grid[x, y] == false } - 1
+				}
+				null to heights
+			} else {
+				// key
+				val heights = (0..<grid.width).map { x ->
+					grid.height - (grid.height - 1 downTo 0).first { y -> grid[x, y] == false } - 2
+				}
+				heights to null
+			}
+		}.let { blocks ->
+			Input(
+				locks = blocks.mapNotNull { it.second },
+				keys = blocks.mapNotNull { it.first }
+			)
+		}
+
+	override fun partOne(data: Input): Long = data.locks.sumOf { lock ->
+			data.keys.count { key ->
+				(lock.zip(key).all {
+					it.second <= 5 - it.first
+				})
+			}.toLong()
+		}
+
+	override fun partTwo(data: Input): Long = 0
+
+}
---
+++ src/test/kotlin/de/earley/adventofcode2024/day25/Day25Test.kt
@@ -0,0 +1,8 @@
+package de.earley.adventofcode2024.day25
+
+import de.earley.adventofcode.testDay
+import io.kotest.core.spec.style.WordSpec
+
+class Day25Test : WordSpec({
+	include(testDay(Day25, 3, 0))
+})
