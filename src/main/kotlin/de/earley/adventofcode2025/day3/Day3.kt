956ca4dad6b094b52c38d2b0bb8760595f354f83
Timothy Earley <timothy.earley@deutschebahn.com>
2025-12-03 06:29:19 +0100

    2025: Day 3 (improved)

--- src/main/kotlin/de/earley/adventofcode2025/day3/Day3.kt
+++ src/main/kotlin/de/earley/adventofcode2025/day3/Day3.kt
@@ -5,40 +5,49 @@ import de.earley.adventofcode.mapToList
 
 fun main() = Day3.start()
 
-private typealias Joltage = Int
+private typealias Joltage = Short
 
 object Day3 : BaseSolution<List<Day3.Bank>, Long, Long>() {
 
 	data class Bank(val batteries : List<Joltage>)
 
-	override fun parseInput(input: Sequence<String>): List<Bank> = input.mapToList { Bank(it.map { it.digitToInt() }) }
+	override fun parseInput(input: Sequence<String>): List<Bank> = input.mapToList {
+		Bank(it.map(Char::digitToInt).map(Int::toShort))
+	}
 
 	override fun partOne(data: List<Bank>): Long = data.sumOf {
-		val max = it.batteries.max()
-		val maxPosition = it.batteries.indexOf(max)
-
-		if (maxPosition == it.batteries.lastIndex) {
-			val secondMaxBefore = it.batteries.slice(0 until maxPosition).max()
-			(secondMaxBefore * 10 + max).toLong()
-		} else {
-			val secondMaxAfter = it.batteries.drop(maxPosition + 1).max()
-			(max * 10 + secondMaxAfter).toLong()
-		}
-
+		it.maxJoltage(2)
 	}
 
 	override fun partTwo(data: List<Bank>): Long = data.sumOf {
+		it.maxJoltage(12)
+	}
+
+	private fun Bank.maxJoltage(numberOfBatteries: Int): Long {
 		var value = 0L
 		var startIndex = 0
-		for (i in 11 downTo  0) {
-			val lastPossibleIndex = it.batteries.lastIndex - i
-			val max = it.batteries.slice(startIndex .. lastPossibleIndex).max()
-			val maxIndex = it.batteries.slice(startIndex .. lastPossibleIndex).indexOf(max) + startIndex
+		for (i in numberOfBatteries - 1 downTo 0) {
+			// Idea: Find the biggest digit after the last number we added while leaving enough for the remaining digits
+			val lastPossibleIndex = batteries.lastIndex - i
+			val (max, maxIndex) = batteries.maxAndIndex(startIndex, lastPossibleIndex)
 			startIndex = maxIndex + 1
 			value *= 10
 			value += max
 		}
-		value
+		return value
+	}
+
+	private fun List<Joltage>.maxAndIndex(after : Int, before: Int): Pair<Joltage, Int> {
+		var max = Short.MIN_VALUE
+		var maxIndex = -1
+		for (i in after .. before) {
+			val value = this[i]
+			if (value > max) {
+				max = value
+				maxIndex = i
+			}
+		}
+		return max to maxIndex
 	}
 
 }
