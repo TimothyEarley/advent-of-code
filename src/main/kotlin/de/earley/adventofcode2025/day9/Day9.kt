e3eefd2489881593b318a2588cbef65fbb3ac154
Timothy Earley <timothy.earley@deutschebahn.com>
2025-12-09 10:46:34 +0100

    2025: Day 9 (failed attempt #2)

--- src/main/kotlin/de/earley/adventofcode2025/day9/Day9.kt
+++ src/main/kotlin/de/earley/adventofcode2025/day9/Day9.kt
@@ -3,7 +3,6 @@ package de.earley.adventofcode2025.day9
 import de.earley.adventofcode.BaseSolution
 import de.earley.adventofcode.Point
 import de.earley.adventofcode.mapToList
-import de.earley.adventofcode.neighbours
 import kotlin.math.abs
 import kotlin.math.max
 import kotlin.math.min
@@ -18,62 +17,91 @@ object Day9 : BaseSolution<List<Point>, Long, Long>() {
 		(abs(it.first.x - it.second.x) + 1).toLong() * (abs(it.first.y - it.second.y) + 1)
 	}
 
+	enum class Shape(
+		val isInteriorRight: Boolean,
+		val isInteriorBottom: Boolean,
+		val isInteriorLeft: Boolean,
+		val isInteriorTop: Boolean,
+	) {
+		/*  xxx
+		 *  xP-
+ 		 *  x|
+ 		 */
+		TopLeftOutside(true, true, true, true),
+
+		/*   P-
+		 *   |x
+		 */
+		TopLeftInside(true, true, false, false),
+
+		/*  xxx
+		 *  -Px
+		 *   |x
+		 */
+		TopRightOutside(true, true, true, true),
+
+		/*   -P
+		 *   x|
+ 		 */
+		TopRightInside(false, true, true, false),
+
+
+		/*    |x
+		 *   -Px
+		 *   xxx
+		 */
+		BottomRightOutside(true, true, true, true),
+
+		/*   x|
+		 *   -P
+		 */
+		BottomRightInside(false, false, true, true),
+
+		/*
+		 *  x|
+		 *  xP-
+		 *  xxx
+		 */
+		BottomLeftOutside(true, true, true, true),
+
+		/*  |x
+		 *  P-
+		 */
+		BottomLeftInside(true, false, false, true),
+	}
+
 	override fun partTwo(data: List<Point>): Long {
 
-		val xStops = data.mapTo(mutableSetOf(), Point::x)
-		val yStops = data.mapTo(mutableSetOf(), Point::y)
+		// Note: This all assumes the points are in clockwise order!
 
-		// the vertical edges are needed for detecting a point in a shape using the basic point in polygon algorithm
-		// since points on the edge count as inside the edge, we expand the shape by one and then do an interior
-		// check on that
+		val pointShapes = data.withIndex().associate { (i, p) ->
+			val prev = data[(i - 1).mod(data.size)]
+			val next = data[(i + 1).mod(data.size)]
 
-		@Suppress("ReplaceRangeToWithRangeUntil") // makes it more consistent here
-		val verticalEdges = (data.zipWithNext() + (data.last() to data.first()))
-			.filter { it.first.x == it.second.x }
-			// create the edge
-			.map { it.first.x to (min(it.first.y, it.second.y) ..  max(it.first.y, it.second.y)) }
-			// move the edge
-			.map { (x, yRange) ->
-				val top = Point(x, yRange.first)
-				val bottom = Point(x, yRange.last)
-				// if there is space above, move up by one
-				val prevTop = data[(data.indexOf(top) - 1).mod(data.size)]
-				val nextTop = data[(data.indexOf(top) + 1).mod(data.size)]
-				val prevBottom = data[(data.indexOf(bottom) - 1).mod(data.size)]
-				val nextBottom = data[(data.indexOf(bottom) + 1).mod(data.size)]
-
-				/*
-				 Where did we come from? Also remember we are winding around the shape clockwise (manually verified)
-
-				      A---F---B
-				          |
-				          |
-        			  C---L---D
-				 */
-
-				// TODO could be nested to reduce redundant checks or split the calulations for new x, top y, and bot y
-				when {
-					// A F L C
-					prevTop.x < top.x && prevBottom == top && nextBottom.x < bottom.x -> (x + 1) to (top.y - 1 .. bottom.y + 1)
-					// A F L D
-					prevTop.x < top.x && prevBottom == top && nextBottom.x > bottom.x -> (x + 1) to (top.y - 1 .. bottom.y - 1)
-					// B F L C
-					prevTop.x > top.x && prevBottom == top && nextBottom.x < bottom.x -> (x + 1) to (top.y + 1 .. bottom.y + 1)
-					// B F L D
-					prevTop.x > top.x && prevBottom == top && nextBottom.x > bottom.x -> (x + 1) to (top.y + 1 .. bottom.y - 1)
-
-					// C L F A
-					prevBottom.x < bottom.x && prevTop == bottom && nextTop.x < top.x -> (x - 1) to (top.y + 1 .. bottom.y - 1)
-					// C L F B
-					prevBottom.x < bottom.x && prevTop == bottom && nextTop.x > top.x -> (x - 1) to (top.y - 1 .. bottom.y - 1)
-					// D L F A
-					prevBottom.x > bottom.x && prevTop == bottom && nextTop.x < top.x -> (x - 1) to (top.y + 1 .. bottom.y + 1)
-					// D L F B
-					prevBottom.x > bottom.x && prevTop == bottom && nextTop.x > top.x -> (x - 1) to (top.y - 1 .. bottom.y + 1)
-
-					else -> error("Should not happen")
+			p to when {
+				prev.y == p.y && prev.x < p.x -> when {
+					next.y < p.y -> Shape.BottomRightOutside
+					else -> Shape.TopRightInside
 				}
+
+				prev.y == p.y && prev.x > p.x -> when {
+					next.y < p.y -> Shape.BottomLeftInside
+					else -> Shape.TopLeftOutside
+				}
+
+				prev.y < p.y -> when {
+					next.x < p.x -> Shape.BottomRightInside
+					else -> Shape.BottomLeftOutside
+				}
+
+				prev.y > p.y -> when {
+					next.x < p.x -> Shape.TopRightOutside
+					else -> Shape.TopLeftInside
+				}
+
+				else -> error("Should not happen")
 			}
+		}
 
 		return data.pairs()
 			.map {
@@ -81,38 +109,40 @@ object Day9 : BaseSolution<List<Point>, Long, Long>() {
 			}.sortedByDescending { it.second }
 			.first { (points, _) ->
 				val (a, b) = points
-				val xRange = min(a.x, b.x) .. max(a.x, b.x)
-				val yRange = min(a.y, b.y) .. max(a.y, b.y)
+				val xRange = min(a.x, b.x)..max(a.x, b.x)
+				val yRange = min(a.y, b.y)..max(a.y, b.y)
 
-				val xToCheck = xStops.filter { it in xRange }
-				val yToCheck = yStops.filter { it in yRange }
+				// check the corners
+				val topLeft = Point(xRange.first, yRange.first)
+				val topRight = Point(xRange.last, yRange.first)
+				val bottomRight = Point(xRange.last, yRange.last)
+				val bottomLeft = Point(xRange.first, yRange.last)
 
-//				val pointsToCheck = ( xToCheck.map { Point(it, a.y) } +
-//					xToCheck.map { Point(it, b.y) } +
-//					yToCheck.map { Point(a.y, it) } +
-//					yToCheck.map { Point(b.y, it) } )
-//					// TODO: why do I need these?
-//					.flatMap { p ->
-//						// add some extra point
-//						p.neighbours(diagonal = true).filter { it.x in xRange && it.y in yRange }.plus(p)
-//					}
+				val corners = (pointShapes[topLeft]?.isInteriorRight ?: true)
+					&& (pointShapes[topLeft]?.isInteriorBottom ?: true)
+					&& (pointShapes[topRight]?.isInteriorLeft ?: true)
+					&& (pointShapes[topRight]?.isInteriorBottom ?: true)
+					&& (pointShapes[bottomRight]?.isInteriorTop ?: true)
+					&& (pointShapes[bottomRight]?.isInteriorLeft ?: true)
+					&& (pointShapes[bottomLeft]?.isInteriorRight ?: true)
+					&& (pointShapes[bottomLeft]?.isInteriorTop ?: true)
 
-				val pointsToCheck = data.flatMap {
-					it.neighbours(diagonal = true).plus(it)
-				}.filter { it.x in xRange && it.y in yRange }
+				val horizontalEdge = data.filter { (it.y == yRange.first || it.y == yRange.last) && it.x > xRange.first && it.x < xRange.last }
+					.all {
+						pointShapes[it]!!.isInteriorLeft && pointShapes[it]!!.isInteriorRight
+					}
+				val verticalEdge = data.filter { (it.x == xRange.first || it.x == xRange.last) && it.y > yRange.first && it.y < yRange.last }
+					.all {
+						pointShapes[it]!!.isInteriorTop && pointShapes[it]!!.isInteriorBottom
+					}
 
-				pointsToCheck.all { p ->
-					verticalEdges.count { (x, yRange) ->
-						// does the edge lie one the path from screen left to the point p?
-						p.x > x && p.y in yRange
-					} % 2 == 1
-				}
-
+				corners && horizontalEdge && verticalEdge
 			}.second
 	}
 
 }
 
+// 4771532800 (part 1)
 // 1559107992
 //   72242816
 
